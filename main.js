1c477ea7fff791f397c0fbfb3e3c5fe01c2666c8
veehan06 <86970245+veehan06@users.noreply.github.com>
2022-08-06 15:45:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    image(video,0,0,300,300)
+}
+function take_snap(){
+    save("FilteredImage");
+}
+function modelLoaded(){
+    console.log("PoseNet is intialized")
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+        console.log("nose X = " + results[0].pose.nose.x);
+        console.log("nose Y = " + results[0].pose.nose.y);
+    }
+}
